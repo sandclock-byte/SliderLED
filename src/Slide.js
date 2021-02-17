b031807b569e4651a749af2cc6664148552fdce2
Fernando Bernal Díaz <fernando.bernal.diaz@gmail.com>
2021-02-17 13:29:52 -0600

    Se creo componente Slide

---
+++ src/Slide.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Slide() {
+    return (
+        <View>
+            <Text>Aquí va el Slide</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
