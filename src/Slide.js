6434de2fc9bf72e2983cf296386a5904427b4d5b
Fernando Bernal Díaz <fernando.bernal.diaz@gmail.com>
2021-02-18 17:14:08 -0600

    Se está trabajando en la trama de LED

--- src/Slide.js
+++ src/Slide.js
@@ -5,12 +5,17 @@ import Slider from '@react-native-community/slider';
 
 export default function Slide() {
 
-    const prevColor = '#4e3fee';
+    const prevTrama = 'R78G63B238W125';
+
+    tramaToColor(prevTrama);
+
+    let prevColor = '#4e3fee';
 
     const picker = useRef();
 
     const [colorLed, setColorLed] = useState('');
     const [textColor, setTextColor] = useState('');
+    const [whiteLed, setWhiteLed] = useState('');
 
     useEffect(() => {
         if (colorLed != '') {
@@ -21,7 +26,13 @@ export default function Slide() {
             picker.current.setColor(prevColor);
             setTextColor(prevColor);
         }
-    }, [colorLed])
+    }, [colorLed]);
+
+    useEffect(() => {
+        if (whiteLed) {
+            console.log(whiteLed);
+        }
+    }, [whiteLed])
 
     return (
         <View>
@@ -43,7 +54,7 @@ export default function Slide() {
                 minimumValue={0}
                 maximumValue={255}
                 onValueChange={(valor) => {
-                    console.log(Math.round(valor));
+                    setWhiteLed(Math.round(valor));
                 }}
                 minimumTrackTintColor="#FFFFFF"
                 maximumTrackTintColor="#000000"
@@ -57,6 +68,31 @@ export default function Slide() {
 const hexToRGBTrama = (hex) => {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return `R${parseInt(result[1], 16)}G${parseInt(result[2], 16)}B${parseInt(result[3], 16)}`;
+}
+
+const componentToHex = (c) => {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+const rgbToHex = (r, g, b) => {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+
+const tramaToColor = (trama) => {
+
+    const indexR = trama.indexOf('R');
+    const indexG = trama.indexOf('G');
+    const indexB = trama.indexOf('B');
+    const indexW = trama.indexOf('W');
+
+    const Red = parseInt(trama.slice(indexR + 1, indexG));
+    const Green = parseInt(trama.slice(indexG + 1, indexB));
+    const Blue = parseInt(trama.slice(indexB + 1, indexW));
+    const White = parseInt(trama.slice(indexW + 1, trama.lenght));
+
+    console.log(rgbToHex(Red, Green, Blue));
 }
 
 const styles = StyleSheet.create({
