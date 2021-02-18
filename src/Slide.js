ada9cd076a6c4fd7e807a12b3454911492ecfb88
Fernando Bernal Díaz <fernando.bernal.diaz@gmail.com>
2021-02-18 13:49:29 -0600

    Se instalo la biblioteca y se está implementando

--- src/Slide.js
+++ src/Slide.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useRef, useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { ColorPicker, ColorPickerRef } from 'react-native-color-picker-light';
 
 export default function Slide() {
+
+    const [colorLed, setColorLed] = useState('');
+
+    useEffect(() => {
+        if (colorLed != '') {
+            // picker.current.setColor(colorLed);
+            console.log(colorLed);
+        }
+    }, [colorLed])
+
+    // const picker = useRef();
+    // console.log(picker);
+    // picker.current.setColor('#f0ce78');
+
+    // console.log(picker);
+
     return (
+
         <View>
-            <Text>Aquí va el Slide</Text>
+            <ColorPicker
+                // ref={picker}
+                type="color"
+                style={{ width: 200, height: 200 }}
+                onColorChange={color => {
+                    setColorLed(color);
+                }}
+            />
+
+            {/* {picker.current.setColor('#b48484')} */}
+
         </View>
+
+
     )
+
 }
 
 const styles = StyleSheet.create({})
